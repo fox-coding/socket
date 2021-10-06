0ec977ac223d64e748656f20f27b815841d4869a
SvLowman <SvenLohmann@gmx.de>
2021-10-06 22:24:49 +0200

    Add messages to message array and display them

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,8 +1,34 @@
-console.log('UI geladen');
+const submitForm = document.getElementById("chat_form");
+const submitInput = document.getElementById("chat_input");
+const chatWrapper = document.getElementById("chat_history");
 
-db.collection('chats').onSnapshot((snapshot)=>{
-    let changes = snapshot.docChanges();
-    changes.forEach(change => {
-        console.log(change.doc.data())
+console.log("UI geladen");
+
+db.collection("chats").onSnapshot((snapshot) => {
+  let changes = snapshot.docChanges();
+  changes.forEach((change) => {
+    var chat = change.doc.data().messages;
+    chatWrapper.innerHTML = "";
+    chat.forEach((message) => {
+      chatWrapper.innerHTML += `<div>${message}</div>`;
     });
-})
+    console.log(change.doc.data());
+  });
+});
+
+submitForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let message = submitInput.value;
+  db.collection("chats")
+    .doc("EQUrVsKLa0A39q7aRkP0")
+    .get()
+    .then((snap) => {
+      let chat = snap.data().messages;
+      chat.push(message);
+      db.collection("chats")
+        .doc("EQUrVsKLa0A39q7aRkP0")
+        .set({ messages: chat });
+    });
+});
+
+// EQUrVsKLa0A39q7aRkP0
